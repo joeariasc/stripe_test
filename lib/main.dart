e24e5a81fb6dab02a0dc9097061513dab93ee4db
Joe Arias <jarias@wawand.co>
2024-02-09 10:22:36 -0500

    adding flutter_dotenv library to store the stripe publishable key used by the mobile app, passing the key to the native app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -33,37 +35,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   static const MethodChannel _channel = MethodChannel('co.wawand/stripe');
   String _result = "Waiting...";
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              _result,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 30, color: Colors.white,),
+        backgroundColor: Colors.black,
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _result,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               ElevatedButton(
-                onPressed: initialize,
-                child: const Text('Get Result')
-                )
-          ],
+                  onPressed: initialize,
+                  child: const Text('Start Payment Process'))
+            ],
           ),
         ),
-    );
+      );
 
-    Future<void> initialize() async {
+  Future<void> initialize() async {
     try {
-      final arguments = {'name': 'Joe Arias'};
+      final arguments = {
+        'stripePublishableKey': dotenv.env['DEFAULT_PUBLISHABLE_KEY']
+      };
       final String newResult = await _channel.invokeMethod('test', arguments);
       setState(() => _result = newResult);
     } on PlatformException catch (e) {
