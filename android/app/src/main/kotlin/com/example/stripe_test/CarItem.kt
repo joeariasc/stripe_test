9077ee7b56cc7e293a36faad97870c61edbdfa5b
Joe Arias <jarias@wawand.co>
2024-02-22 10:21:38 -0500

    displaying items with price in recycler view, adding logic to redirect to the flutter page

---
+++ android/app/src/main/kotlin/com/example/stripe_test/CarItem.kt
@@ -0,0 +1,6 @@
+package com.example.stripe_test
+
+data class CartItem(
+    val name: String,
+    val price: String
+)
--- android/app/src/main/kotlin/com/example/stripe_test/CheckoutActivity.kt
+++ android/app/src/main/kotlin/com/example/stripe_test/CheckoutActivity.kt
@@ -7,12 +7,19 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stripe_test.adapters.CartItemsAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.stripe.android.PaymentConfiguration
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
 import com.stripe.android.paymentsheet.addresselement.AddressDetails
 import com.stripe.android.paymentsheet.addresselement.AddressLauncher
 import com.stripe.android.paymentsheet.addresselement.AddressLauncherResult
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.plugin.common.MethodChannel
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaType
@@ -29,7 +36,12 @@ class CheckoutActivity : AppCompatActivity() {
         private const val TAG = "CheckoutActivity"
     }
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var cartItemsAdapter: CartItemsAdapter
+
     private lateinit var backedUrl: String
+    private lateinit var paymentAmount: String
+    private lateinit var itemsList: List<List<String>>
 
     private lateinit var paymentIntentClientSecret: String
     private lateinit var paymentSheet: PaymentSheet
@@ -44,22 +56,37 @@ class CheckoutActivity : AppCompatActivity() {
 
     private lateinit var publishableKey: String
 
-    private val addressConfiguration =
-        AddressLauncher.Configuration(
-            additionalFields = AddressLauncher.AdditionalFieldsConfiguration(
-                phone = AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.REQUIRED
-            ),
-            allowedCountries = setOf("US", "CA", "GB"),
-            title = "Shipping Address",
+    private val addressConfiguration = AddressLauncher.Configuration(
+        additionalFields = AddressLauncher.AdditionalFieldsConfiguration(
+            phone = AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.REQUIRED
+        ),
+        allowedCountries = setOf("US", "CA", "GB"),
+        title = "Shipping Address",
 
-            )
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
 
+        recyclerView = findViewById(R.id.cart_items_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val carItems = mutableListOf<CartItem>()
+
+        intent.getStringExtra(JSON_CAR_ITEMS)?.let { cartItemsJson ->
+            val type = object : TypeToken<List<List<String>>>() {}.type
+            itemsList = Gson().fromJson(cartItemsJson, type)
+            Log.d(TAG, "itemsList => $itemsList")
+            itemsList.map { carItems.add(CartItem(it[0], it[1])) }
+        }
+
+        cartItemsAdapter = CartItemsAdapter(carItems)
+        recyclerView.adapter = cartItemsAdapter
+
         publishableKey = intent.getStringExtra(PUBLISHABLE_KEY) ?: ""
         backedUrl = intent.getStringExtra(BACKEND_URL) ?: ""
+        paymentAmount = intent.getStringExtra(AMOUNT) ?: ""
 
         PaymentConfiguration.init(applicationContext, publishableKey)
 
@@ -87,22 +114,18 @@ class CheckoutActivity : AppCompatActivity() {
             {
                 "items": [
                     {"id":"xl-tshirt"}
-                ]
+                ],
+                "amount": "$paymentAmount"
             }
         """
 
         val mediaType = "application/json; charset=utf-8".toMediaType()
 
         val body = shoppingCartContent.toRequestBody(mediaType)
-        val request = Request.Builder()
-            .url(url)
-            .post(body)
-            .build()
+        val request = Request.Builder().url(url).post(body).build()
 
-        val client = OkHttpClient.Builder()
-            .connectTimeout(20, TimeUnit.SECONDS)
-            .readTimeout(20, TimeUnit.SECONDS)
-            .build()
+        val client = OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS).build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -125,9 +148,7 @@ class CheckoutActivity : AppCompatActivity() {
 
     private fun showAlert(title: String, message: String? = null) {
         runOnUiThread {
-            val builder = AlertDialog.Builder(this)
-                .setTitle(title)
-                .setMessage(message)
+            val builder = AlertDialog.Builder(this).setTitle(title).setMessage(message)
             builder.setPositiveButton("Ok", null)
             builder.create().show()
         }
@@ -148,8 +169,7 @@ class CheckoutActivity : AppCompatActivity() {
 
     private fun onAddressClicked(view: View) {
         addressLauncher.present(
-            publishableKey = publishableKey,
-            configuration = addressConfiguration
+            publishableKey = publishableKey, configuration = addressConfiguration
         )
     }
 
@@ -157,6 +177,14 @@ class CheckoutActivity : AppCompatActivity() {
         when (paymentResult) {
             is PaymentSheetResult.Completed -> {
                 showToast("Payment complete!")
+                val flutterEngine = FlutterEngineCache.getInstance().get("my_engine_id")
+                if (flutterEngine != null) {
+                    MethodChannel(
+                        flutterEngine.dartExecutor,
+                        METHOD_CHANNEL_NAME
+                    ).invokeMethod("paymentSuccess", null)
+                }
+                finish()
             }
 
             is PaymentSheetResult.Canceled -> {
