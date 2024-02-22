4d4f524759c90af43dc9b03623756de5a3fcf7f8
Joe Arias <jarias@wawand.co>
2024-02-22 15:12:59 -0500

    declaring stripeChannel as general variable, getting items from the arguemnts and enabling navigation to the previous screen after complete the payment

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,6 +6,8 @@ import Flutter
     
     var navigationController: UINavigationController!
     
+    var stripeChannel: FlutterMethodChannel!
+    
     override func application(
         
         _ application: UIApplication,
@@ -32,21 +34,23 @@ extension AppDelegate {
     private func setupMethodChannelForStripe(controller: FlutterViewController){
         let METHOD_CHANNEL_NAME = "co.wawand/stripe"
         
-        let stripeChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: controller.binaryMessenger)
+        stripeChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: controller.binaryMessenger)
         
         stripeChannel.setMethodCallHandler({
             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
             switch call.method {
             case "test":
-                guard let args = call.arguments as? [String: String] else {return}
-                let stripePublishableKey = args["stripePublishableKey"]!
-                let backendURL = args["serverHost"]!
-                let amount = args["amount"]!
+                guard let args = call.arguments as? [String: Any] else {return}
+                let stripePublishableKey = args["stripePublishableKey"] as! String
+                let backendURL = args["serverHost"] as! String
+                let amount = args["amount"] as! String
+                let cartItems = args["cartItems"] as! [[String]]
 
                 let vc = UIStoryboard.init(name: "Main", bundle: .main).instantiateViewController(identifier: "CheckoutViewController") as! CheckoutViewController
                 vc.stripePublishableKey = stripePublishableKey
                 vc.serverHost = backendURL
                 vc.amount = amount
+                vc.cartItems = cartItems
                 vc.result = result
                 self.navigationController.pushViewController(vc, animated: true)
             default:
